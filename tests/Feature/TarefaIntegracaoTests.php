64237889608679cbb717a86f23587c1ac3f5d99e
casagrande7 <joaofootz10@gmail.com>
2024-10-01 08:57:28 -0300

    Commit Inicial

---
+++ tests/Feature/TarefaIntegracaoTests.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TarefaIntegracaoTests extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_criar_tarefa_por_meio_da_api()
+    {
+        
+    }
+}
